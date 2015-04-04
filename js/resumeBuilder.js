374a68e89718210e08869126b943502eb3b2797e
David Christensen <David.Christensen@escocorp.com>
2015-04-04 09:02:34 -0700

    Changed to match instructions in readme.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,19 +1,28 @@
 var skills = ["C#", "C++", "NXOpen", "Other stuff"];
 
-var bio = {
-	"name" : "David Christensen",
-	"role" : "Design Engineer",
+
+var contact1 = {
 	"mobile" : "123-456-7890",
 	"email" : "email@gmail.com",
 	"github" : "https://github.com/davidmchris",
-	"pictureURL" : "images/dave.jpg",
-	"welcomeMessage" : "Hello",
-	"skills" : skills
+	"twitter" : "None",
+	"location" : "Some address",
+  
 };
+
+var bio = {
+	"name" : "David Christensen",
+	"role" : "Design Engineer",
+  "contacts" : contact1,
+	"welcomeMessage" : "Hello",
+	"skills" : skills,
+  "biopic" : "images/dave.jpg"
+};
+
 
 $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
 $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-$("#topContacts").append(HTMLmobile.replace("%data%", bio.mobile))
-$("#topContacts").append(HTMLemail.replace("%data%", bio.email))
-$("#topContacts").append(HTMLgithub.replace("%data%", bio.github))
-$("#header").append(HTMLbioPic.replace("%data%", bio.pictureURL))
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile))
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email))
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github))
+$("#header").append(HTMLbioPic.replace("%data%", bio.biopic))
