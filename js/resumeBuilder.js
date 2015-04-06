187a9611da57042ceb592375e51fd351c57e3ef0
dchriste <david.christensen@gmail.com>
2015-04-06 11:45:20 -0700

    Make resume builder with JSON format.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,28 +1,99 @@
 var skills = ["C#", "C++", "NXOpen", "Other stuff"];
 
 
-var contact1 = {
-	"mobile" : "123-456-7890",
-	"email" : "email@gmail.com",
-	"github" : "https://github.com/davidmchris",
-	"twitter" : "None",
-	"location" : "Some address",
-  
-};
 
 var bio = {
 	"name" : "David Christensen",
 	"role" : "Design Engineer",
-  "contacts" : contact1,
+  "contacts" : [{
+    "mobile" : "123-456-7890",
+    "email" : "email@gmail.com",
+    "github" : "https://github.com/davidmchris",
+    "twitter" : "None",
+    "location" : "Some place"}],
 	"welcomeMessage" : "Hello",
-	"skills" : skills,
+	"skills" : ["C++","C#","NX","Web services"],
   "biopic" : "images/dave.jpg"
 };
 
+var work = {
+  "jobs" : [ 
+  {
+       "employer" : "ESCO Corporation", 
+       "title" : "Design Engineer", 
+       "location" : "Portland, OR", 
+       "dates" : "April 2012 - Current",
+       "description" : "Design and write software to automate the engineering process." 
+  }
+  ]
+}
 
-$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile))
-$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email))
-$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github))
-$("#header").append(HTMLbioPic.replace("%data%", bio.biopic))
+var projects = {
+  "projects": [
+    {
+      "title": "Plate Tool",
+      "dates": "Apr 2014 - Aug 2014",
+      "description": "Created a tool that reduces design time from 14 weeks to 4 weeks.",
+      "images": ["placeholder.it/75x75"]
+    }
+  ]
+}
+
+var education = {
+  "schools" : 
+  [
+    {
+      "name": "BYU-Idaho",
+      "location": "Rexburg, ID",
+      "degree": "None",
+      "majors": ["Mechanical Engineering"],
+      "dates": 2007,
+      "url": "www.byui.edu"
+    },
+    {
+      "name": "BYU-Provo",
+      "location": "Provo, UT",
+      "degree": "BS, MS",
+      "majors": ["Mechanical Engineering"],
+      "dates": 2012,
+      "url": "www.byu.edu"
+    }
+  ],
+  "onlineCourses": 
+  [
+    {
+      "title": "Software Development Lifecycles",
+      "school": "Udacity",
+      "date": 2015,
+      "url": "www.udacity.com"
+    },
+    {
+      "title": "Intro to HTML and CSS",
+      "school": "Udacity",
+      "date": 2015,
+      "url": "www.udacity.com"
+    },
+    {
+      "title": "JavaScript Basics",
+      "school": "Udacity",
+      "date": 2015,
+      "url": "www.udacity.com"
+    }
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
